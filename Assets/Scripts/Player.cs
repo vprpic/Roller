3e9444d3adf34b6d750dcc4d38afdc5ef329875a
Vinka Prpić <vinka.prpic@gmail.com>
2018-05-25 18:33:57 +0200

    Added tap controls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     public float rotationSpeed;
+    public TapTest tapTest;
     
 	void Start () {
 	}
 	
 	void Update () {
         transform.Rotate(rotationSpeed * Time.deltaTime, 0, 0);
-	}
+        if (tapTest.Tap)
+        {
+            Debug.Log("Tap!");
+        }
+    }
 }
